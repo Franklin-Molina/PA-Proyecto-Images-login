9fd4027331dcc54719ecaf63e6538787f74ec2b1
Franklin-Molina <asdfglxl7@gmail.com>
2022-03-28 20:06:37 -0500

    'validaciones-password'

--- src/controllers/create_user.py
+++ src/controllers/create_user.py
@@ -4,7 +4,7 @@ from src import app
 from werkzeug.security import generate_password_hash,check_password_hash
 app.secret_key='15sdf153f525a1d'
 
-
+import re
 
         
 @app.route("/createuser", methods =['GET','POST'])
@@ -15,15 +15,49 @@ def createUser():
                 password=request.form.get('password')
                 encriptar= generate_password_hash(password)                 
                 isValid=True
+
+                lenpass =  len(password)
+                minuscula = False
+                mayuscula= False
+                numeros = False
+                special = False
+
                 if name == "":
                         flash("Nombre Obligatorio")
                         isValid=False        
                 if email == "":
                         flash("Correo Obligatorio")   
-                        isValid=False        
+                        isValid=False    
+                
                 if password == "":
                         flash("Contraseña Obligatoria")  
                         isValid=False  
+                if lenpass < 8 :                       
+                        flash("La contraseña debe tener minimo 8 caracteres")                       
+                        isValid=False 
+               
+                
+                for caracter in password:
+                        if caracter.islower()== True:
+                                minuscula= True
+                        if caracter.isupper()== True:
+                                mayuscula= True
+                        if caracter.isdigit()==True:
+                                numeros= True                   
+                               
+                if minuscula == False:
+                        flash("Ingrese al menos una Minuscula a la contraseña")
+                if mayuscula == False:
+                        flash("Ingrese al menos una Mayusculas a la contraseña")
+                if numeros == False:
+                        flash("Ingrese al menos un Número a la contraseña")
+                
+                if re.search('[@_!#$%^&*()<>?/\|}{~:]',password):                        
+                        special= True
+                       
+                if special == False:
+                        flash("Ingrese al menos un caracter a la contraseña")
+
                 if isValid==False:
                         return render_template("CreateUser.html",name=name,email=email,password=password)
         
